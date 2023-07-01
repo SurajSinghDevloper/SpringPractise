0af8bcf7275ee9f85d0bc180a5fd1def7e424e05
Suraj Kumar <suraj31kumar1999@gmail.com>
2023-07-01 23:39:50 +0530

    p-schema and value as attribute

--- SpringCore/demo/src/main/java/com/springcore/App.java
+++ SpringCore/demo/src/main/java/com/springcore/App.java
@@ -11,7 +11,12 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        // here get bean is returning the object so i have stored in Student var..
         Student student1 = (Student) context.getBean("student1");
+        Student student2 = (Student) context.getBean("student2");
+        Student student3 = (Student) context.getBean("student3");
         System.out.println(student1);
+        System.out.println(student2);
+        System.out.println(student3);
     }
 }
